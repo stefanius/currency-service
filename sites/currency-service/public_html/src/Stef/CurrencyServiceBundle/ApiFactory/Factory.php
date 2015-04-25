ba19ac63873256ba01497564a26eccb83db19e73
stefanius <KIT_kat_16>
2015-04-26 00:43:15 +0200

    Added and implemented Factory service

---
+++ sites/currency-service/public_html/src/Stef/CurrencyServiceBundle/ApiFactory/Factory.php
@@ -0,0 +1,33 @@
+<?php
+namespace Stef\CurrencyServiceBundle\ApiFactory;
+
+use Stef\CurrencyServiceBundle\ApiConnectors\ConnectorInterface;
+use Stef\CurrencyServiceBundle\ApiConnectors\CurrencyConverterKowabungaConnector;
+use Stef\CurrencyServiceBundle\ApiConnectors\WebservicexConnector;
+
+class Factory
+{
+    /**
+     * @param $service
+     *
+     * @return ConnectorInterface
+     */
+    public function getConverter($service)
+    {
+        if ($service === 'kowabunga') {
+            $client = new \SoapClient('http://currencyconverter.kowabunga.net/converter.asmx?WSDL');
+            $converter = new CurrencyConverterKowabungaConnector($client);
+
+            return $converter;
+        }
+
+        if ($service === 'webservicex') {
+            $client = new \SoapClient('http://www.webservicex.net/currencyconvertor.asmx?WSDL');
+            $converter = new WebservicexConnector($client);
+
+            return $converter;
+        }
+
+        return null;
+    }
+}
